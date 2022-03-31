12ceac76ca1a734d23b6339409146e30c114c33a
tiankun <tiankun@kuaishou.com>
2022-03-31 22:29:02 +0800

    修改less的使用方式

--- app/components/Cart(Done)/detail.jsx
+++ app/components/Cart(Done)/detail.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import styles from "../Cart(Done)/detail.less";
 
 export default class CartsList extends Component {
     constructor(props) {
@@ -93,8 +94,8 @@ export default class CartsList extends Component {
 
         return (
             <div>
-                <div class="row">
-                    <div class="col-md-12">
+                <div className={styles["row"]}>
+                    <div className={styles["col-md-12"]}>
                         <ol class="breadcrumb">
                             <li><a href="{ route('home') }"> <i class="fa fa-home"></i> Home</a></li>
                             <li class="active">Cart</li>
@@ -102,9 +103,9 @@ export default class CartsList extends Component {
                     </div>
                 </div>
 
-                <div class="row">
-                    <div class="col-md-12 content">
-                        <div class="box-body">
+                <div className={styles["row"]}>
+                    <div className={styles["col-md-12 content"]}>
+                        <div className={styles["box-body"]}>
                             @include('layouts.errors-and-messages')
                         </div>
                         <h3><i class="fa fa-cart-plus"></i> Shopping Cart</h3>
@@ -112,41 +113,41 @@ export default class CartsList extends Component {
                 </div>
 
 
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="row hidden-xs hidden-sm" style="height: 40px;">
+                <div className={styles["row"]}>
+                    <div className={styles["col-md-12"]}>
+                        <div className={styles["row hidden-xs hidden-sm"]} style="height: 40px;">
 
-                            <div class="col-lg-2 col-md-2 col-sm-4 col-xs-4">
-                                <div class="row">
-                                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12"><b>Cover</b></div>
+                            <div className={styles["col-lg-2 col-md-2 col-sm-4 col-xs-4"]}>
+                                <div className={styles["row"]}>
+                                    <div className={styles["col-lg-12 col-md-12 col-sm-12 col-xs-12"]}><b>Cover</b></div>
                                 </div>
                             </div>
 
-                            <div class="col-lg-10 col-md-10 col-sm-8 col-xs-8">
-                                <div class="row">
-                                    <div class="col-lg-5 col-md-5"><b>Name</b></div>
-                                    <div class="col-lg-2 col-md-2"><b>Quantity</b></div>
-                                    <div class="col-lg-1 col-md-1"><b>Remove</b></div>
-                                    <div class="col-lg-2 col-md-2"><b>Price</b></div>
-                                    <div class="col-lg-2 col-md-2"><b>Total</b></div>
+                            <div className={styles["col-lg-10 col-md-10 col-sm-8 col-xs-8"]}>
+                                <div className={styles["row"]}>
+                                    <div className={styles["col-lg-5 col-md-5"]}><b>Name</b></div>
+                                    <div className={styles["col-lg-2 col-md-2"]}><b>Quantity</b></div>
+                                    <div className={styles["col-lg-1 col-md-1"]}><b>Remove</b></div>
+                                    <div className={styles["col-lg-2 col-md-2"]}><b>Price</b></div>
+                                    <div className={styles["col-lg-2 col-md-2"]}><b>Total</b></div>
                                 </div>
                             </div>
                         </div>
-                        <div class="row">
-                            <div class="col-lg-2 col-md-2 col-sm-3 col-xs-4">
-                                <div class="row">
-                                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
+                        <div className={styles["row"]}>
+                            <div className={styles["col-lg-2 col-md-2 col-sm-3 col-xs-4"]}>
+                                <div className={styles["row"]}>
+                                    <div className={styles["col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center"]}>
                                         <a href="{ route('front.get.cart', [cartItem.cart.slug]) }" class="hover-border">
                                             <img src="{cartItem.cover}" alt="{ cartItem.name }" class="img-responsive img-thumbnail"></img>
                                         </a>
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-lg-10 col-md-10 col-sm-9 col-xs-8">
-                                <div class="row">
+                            <div className={styles["col-lg-10 col-md-10 col-sm-9 col-xs-8"]}>
+                                <div className={styles["row"]}>
 
 
-                                    <div class="col-lg-5 col-md-5 col-sm-12 col-xs-12">
+                                    <div className={styles["col-lg-5 col-md-5 col-sm-12 col-xs-12"]}>
                                         <h4 style="margin-bottom:5px;">{cartItem.name}</h4>
                                         @if(cartItem.options.has('combination'))
                                         <div style="margin-bottom:5px;">
@@ -158,17 +159,17 @@ export default class CartsList extends Component {
                                         {cartItem.cart.description}
                                     </div>
 
-                                    <div class="col-lg-2 col-md-2 col-sm-4 col-xs-8">
+                                    <div className={styles["col-lg-2 col-md-2 col-sm-4 col-xs-8"]}>
                                         <form action="{ route('cart.update', cartItem.rowId) }" class="form-inline" method="post">
                                             {csrf_field()}
                                             <input type="hidden" name="_method" value="put"></input>
-                                            <div class="input-group">
+                                            <div className={styles["input-group"]}>
                                                 <input type="text" name="quantity" value="{ cartItem.qty }" class="form-control input-sm" />
                                                 <span class="input-group-btn"><button class="btn btn-default btn-sm">Update</button></span>
                                             </div>
                                         </form>
                                     </div>
-                                    <div class="col-lg-1 col-md-1 col-sm-8 col-xs-4">
+                                    <div className={styles["col-lg-1 col-md-1 col-sm-8 col-xs-4"]}>
                                         <form action="{ route('cart.destroy', cartItem.rowId) }" method="post">
                                             {csrf_field()}
                                             <input type="hidden" name="_method" value="delete"></input>
